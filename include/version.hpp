a5eca184fa52a6792529377ee74a5ab3089e8aeb
Dirk Marciniak <dirk.marciniak@mail.de>
2022-12-27 17:36:37 +0100

    -marker für aktuelle version

--- include/version.hpp
+++ include/version.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-constexpr const char *MY_APP_VERSION{"<a87937f> 2022-12-26 17:13:25 +0100 dirk.marciniak@mail.de"};
+constexpr const char *MY_APP_VERSION{"<1f0ee95> 2022-12-27 17:34:52 +0100 dirk.marciniak@mail.de"};
 
