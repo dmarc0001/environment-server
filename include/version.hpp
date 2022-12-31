03aef90d5f7d3f3f62c81fd25facd8270f5f40f1
Dirk Marciniak <dirk.marciniak@mail.de>
2022-12-31 15:20:54 +0100

    - filesys chceker aufgeräumt

--- include/version.hpp
+++ include/version.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-constexpr const char *MY_APP_VERSION{"<27ae591> 2022-12-30 20:35:35 +0100 dirk.marciniak@mail.de"};
+constexpr const char *MY_APP_VERSION{"<ad4d854> 2022-12-31 14:56:13 +0100 dirk.marciniak@mail.de"};
 
