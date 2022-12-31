ad4d85482cbaf72e230ed51ac35d4991c79ab339
Dirk Marciniak <dirk.marciniak@mail.de>
2022-12-31 14:56:13 +0100

    - sollte stabil laufen

--- include/version.hpp
+++ include/version.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-constexpr const char *MY_APP_VERSION{"<1f0ee95> 2022-12-27 17:34:52 +0100 dirk.marciniak@mail.de"};
+constexpr const char *MY_APP_VERSION{"<27ae591> 2022-12-30 20:35:35 +0100 dirk.marciniak@mail.de"};
 
