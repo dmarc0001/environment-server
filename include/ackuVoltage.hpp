3b832f477b35f52532d1dbf7aecac2cdf6ca0052
Dirk Marciniak <dirk.marciniak@mail.de>
2023-01-02 09:50:43 +0100

    Erste Versiuche Umstellung IDF 5.0

--- include/ackuVoltage.hpp
+++ include/ackuVoltage.hpp
@@ -2,14 +2,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-// #include <freertos/FreeRTOS.h>
-// #include <freertos/task.h>
-// #include <driver/adc.h>
-// #include <esp_adc_cal.h>
-
 #include "appPreferences.hpp"
 
-//ADC Calibration
+// ADC Calibration
 #if CONFIG_IDF_TARGET_ESP32
 #define ADC_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_VREF
 #elif CONFIG_IDF_TARGET_ESP32S2
@@ -26,17 +21,17 @@ namespace webserver
 {
   class AckuVoltage
   {
-  private:
+    private:
     static const char *tag;
-    static bool is_running; //! is the task running
+    static bool is_running;  //! is the task running
     static esp_adc_cal_characteristics_t adc1_chars;
 
-  public:
+    public:
     static void start();
 
-  private:
-    static void ackuTask(void *);
+    private:
+    static void ackuTask( void * );
     static bool adcCalibrationInit();
   };
 
-}
+}  // namespace webserver
--- main/ackuVoltage.cpp
+++ main/ackuVoltage.cpp
@@ -5,8 +5,9 @@
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <driver/adc.h>
-#include <esp_adc_cal.h>
+#include <esp_adc/adc_continuous.h>
+#include <esp_adc/adc_cali.h>
+#include <esp_adc/adc_cali_scheme.h>
 
 #include "ackuVoltage.hpp"
 #include "statusObject.hpp"
@@ -17,155 +18,156 @@ namespace webserver
 
   using namespace Prefs;
 
-  const char *AckuVoltage::tag{"acku_voltage"};
-  bool AckuVoltage::is_running{false};
+  const char *AckuVoltage::tag{ "acku_voltage" };
+  bool AckuVoltage::is_running{ false };
   esp_adc_cal_characteristics_t AckuVoltage::adc1_chars;
 
   /**
    * start low prio task for scanning the acku value
-  */
+   */
   void AckuVoltage::start()
   {
-    if (AckuVoltage::is_running)
+    if ( AckuVoltage::is_running )
     {
-      ESP_LOGE(AckuVoltage::tag, "acku measure task is running, abort.");
+      ESP_LOGE( AckuVoltage::tag, "acku measure task is running, abort." );
     }
     else
     {
       // for security, if done do nothing
       StatusObject::init();
-      xTaskCreate(AckuVoltage::ackuTask, "acku-measure", configMINIMAL_STACK_SIZE * 4, NULL, tskIDLE_PRIORITY, NULL);
+      xTaskCreate( AckuVoltage::ackuTask, "acku-measure", configMINIMAL_STACK_SIZE * 4, NULL, tskIDLE_PRIORITY, NULL );
     }
   }
 
   /**
    * task for scanning the acku value. if no value to measure, deactivate
-  */
-  void AckuVoltage::ackuTask(void *)
+   */
+  void AckuVoltage::ackuTask( void * )
   {
     AckuVoltage::is_running = true;
-    static uint32_t measures[CURRENT_SMOOTH_COUNT];
+    static uint32_t measures[ CURRENT_SMOOTH_COUNT ];
     bool cali_enable = AckuVoltage::adcCalibrationInit();
     uint32_t idx = 0;
 
     // fill array with defaults
-    for (auto i = 0; i < CURRENT_SMOOTH_COUNT; i++)
+    for ( auto i = 0; i < CURRENT_SMOOTH_COUNT; i++ )
     {
-      measures[i] = ACKU_LOWER_VALUE;
+      measures[ i ] = ACKU_LOWER_VALUE;
     }
-    vTaskDelay(pdMS_TO_TICKS(234));
+    vTaskDelay( pdMS_TO_TICKS( 234 ) );
     // infinity loop
-    while (true)
+    while ( true )
     {
-      adc_power_acquire();
-      vTaskDelay(pdMS_TO_TICKS(1));
-      uint32_t voltage{0U};
-      int adc_raw = adc1_get_raw(ACKU_MEASURE_CHANNEL);
-      //ESP_LOGD(AckuVoltage::tag, "raw  data: %d", adc_raw);
-      if (cali_enable)
+      // obsolete : adc_power_acquire();
+      vTaskDelay( pdMS_TO_TICKS( 1 ) );
+      uint32_t voltage{ 0U };
+      int adc_raw = adc1_get_raw( ACKU_MEASURE_CHANNEL );
+      // ESP_LOGD(AckuVoltage::tag, "raw  data: %d", adc_raw);
+      if ( cali_enable )
       {
         //
         // the measure is on R-Bridge a 100k, the value is the
         // half of the current value
         //
-        measures[idx] = 2 * esp_adc_cal_raw_to_voltage(adc_raw, &AckuVoltage::adc1_chars);
-        for (auto i = 0; i < CURRENT_SMOOTH_COUNT; i++)
+        measures[ idx ] = 2 * esp_adc_cal_raw_to_voltage( adc_raw, &AckuVoltage::adc1_chars );
+        for ( auto i = 0; i < CURRENT_SMOOTH_COUNT; i++ )
         {
-          voltage += measures[i];
+          voltage += measures[ i ];
         }
-        voltage = static_cast<uint32_t>(floor(voltage / CURRENT_SMOOTH_COUNT));
-        ESP_LOGD(AckuVoltage::tag, "acku smooth: %d mV (measured %d raw: %d)", voltage, measures[idx], adc_raw);
-        StatusObject::setVoltage(voltage);
+        voltage = static_cast< uint32_t >( floor( voltage / CURRENT_SMOOTH_COUNT ) );
+        ESP_LOGD( AckuVoltage::tag, "acku smooth: %d mV (measured %d raw: %d)", voltage, measures[ idx ], adc_raw );
+        StatusObject::setVoltage( voltage );
         ++idx;
-        if (idx >= CURRENT_SMOOTH_COUNT)
+        if ( idx >= CURRENT_SMOOTH_COUNT )
         {
           idx = 0U;
         }
       }
       else
       {
-        StatusObject::setVoltage(0);
+        StatusObject::setVoltage( 0 );
       }
-      adc_power_release();
+      // obsolete : adc_power_release();
       //
       // write a acku log file
       //
-      if (!StatusObject::getIsBrownout() && (StatusObject::getWlanState() == WlanState::TIMESYNCED))
+      if ( !StatusObject::getIsBrownout() && ( StatusObject::getWlanState() == WlanState::TIMESYNCED ) )
       {
         // write acku value to logfile
-        std::string fileName(Prefs::ACKU_LOG_FILE);
-        ESP_LOGD(AckuVoltage::tag, "acku value to file <%s>)", fileName.c_str());
+        std::string fileName( Prefs::ACKU_LOG_FILE );
+        ESP_LOGD( AckuVoltage::tag, "acku value to file <%s>)", fileName.c_str() );
         //
         // if filesystemchecker want to write, prevent this
         // read is walways possible
         //
-        if (xSemaphoreTake(StatusObject::fileSem, pdMS_TO_TICKS(1000)) == pdTRUE)
+        if ( xSemaphoreTake( StatusObject::fileSem, pdMS_TO_TICKS( 1000 ) ) == pdTRUE )
         {
-          auto aFile = fopen(fileName.c_str(), "a");
-          if (aFile)
+          auto aFile = fopen( fileName.c_str(), "a" );
+          if ( aFile )
           {
             timeval val;
-            gettimeofday(&val, nullptr);
+            gettimeofday( &val, nullptr );
             auto timestamp = val.tv_sec;
             // write value to file with timestamp
             cJSON *dataSetObj = cJSON_CreateObject();
             // make timestamp objekt item
-            cJSON_AddItemToObject(dataSetObj, Prefs::JSON_TIMESTAMP_NAME, cJSON_CreateString(std::to_string(timestamp).c_str()));
-            cJSON_AddItemToObject(dataSetObj, Prefs::JSON_ACKU_CURRENT_NAME, cJSON_CreateString(std::to_string(voltage).c_str()));
-            char *jsonPrintString = cJSON_PrintUnformatted(dataSetObj);
-            fputs(jsonPrintString, aFile);
-            fflush(aFile);
-            fputs("\n", aFile);
-            fclose(aFile);
-            cJSON_Delete(dataSetObj);
-            cJSON_free(jsonPrintString); // !!!!!!! memory leak if not
+            cJSON_AddItemToObject( dataSetObj, Prefs::JSON_TIMESTAMP_NAME, cJSON_CreateString( std::to_string( timestamp ).c_str() ) );
+            cJSON_AddItemToObject( dataSetObj, Prefs::JSON_ACKU_CURRENT_NAME,
+                                   cJSON_CreateString( std::to_string( voltage ).c_str() ) );
+            char *jsonPrintString = cJSON_PrintUnformatted( dataSetObj );
+            fputs( jsonPrintString, aFile );
+            fflush( aFile );
+            fputs( "\n", aFile );
+            fclose( aFile );
+            cJSON_Delete( dataSetObj );
+            cJSON_free( jsonPrintString );  // !!!!!!! memory leak if not
           }
-          xSemaphoreGive(StatusObject::fileSem);
+          xSemaphoreGive( StatusObject::fileSem );
         }
       }
       //
       // sleep for a while, acku needs som time for changing
       //
-      //vTaskDelay(pdMS_TO_TICKS(1003));
-      vTaskDelay(pdMS_TO_TICKS(45013));
+      // vTaskDelay(pdMS_TO_TICKS(1003));
+      vTaskDelay( pdMS_TO_TICKS( 45013 ) );
     }
   }
 
   /**
-   * init calibration 
+   * init calibration
    * https://github.com/espressif/esp-idf/blob/v4.4/examples/peripherals/adc/single_read/single_read/main/single_read.c
-  */
+   */
   bool AckuVoltage::adcCalibrationInit()
   {
     using namespace Prefs;
     esp_err_t ret;
     bool cali_enable = false;
 
-    ESP_LOGI(AckuVoltage::tag, "init adc calibration...");
-    adc_power_acquire();
-    vTaskDelay(pdMS_TO_TICKS(1));
-    ret = esp_adc_cal_check_efuse(ADC_CALI_SCHEME);
-    adc1_config_width(ACKU_ADC_WIDTH);
-    adc1_config_channel_atten(ACKU_MEASURE_CHANNEL, ACKU_ATTENT);
-    if (ret == ESP_ERR_NOT_SUPPORTED)
+    ESP_LOGI( AckuVoltage::tag, "init adc calibration..." );
+    // obsolete adc_power_acquire();
+    vTaskDelay( pdMS_TO_TICKS( 1 ) );
+    ret = esp_adc_cal_check_efuse( ADC_CALI_SCHEME );
+    adc1_config_width( ACKU_ADC_WIDTH );
+    adc1_config_channel_atten( ACKU_MEASURE_CHANNEL, ACKU_ATTENT );
+    if ( ret == ESP_ERR_NOT_SUPPORTED )
     {
-      ESP_LOGW(AckuVoltage::tag, "Calibration scheme not supported, skip software calibration");
+      ESP_LOGW( AckuVoltage::tag, "Calibration scheme not supported, skip software calibration" );
     }
-    else if (ret == ESP_ERR_INVALID_VERSION)
+    else if ( ret == ESP_ERR_INVALID_VERSION )
     {
-      ESP_LOGW(AckuVoltage::tag, "eFuse not burnt, skip software calibration");
+      ESP_LOGW( AckuVoltage::tag, "eFuse not burnt, skip software calibration" );
     }
-    else if (ret == ESP_OK)
+    else if ( ret == ESP_OK )
     {
       cali_enable = true;
-      esp_adc_cal_characterize(ADC_UNIT_1, ACKU_ATTENT, ACKU_ADC_WIDTH, 0, &AckuVoltage::adc1_chars);
-      ESP_LOGI(AckuVoltage::tag, "init adc calibration successful...");
+      esp_adc_cal_characterize( ADC_UNIT_1, ACKU_ATTENT, ACKU_ADC_WIDTH, 0, &AckuVoltage::adc1_chars );
+      ESP_LOGI( AckuVoltage::tag, "init adc calibration successful..." );
     }
     else
     {
-      ESP_LOGE(AckuVoltage::tag, "Invalid arg");
+      ESP_LOGE( AckuVoltage::tag, "Invalid arg" );
     }
-    adc_power_release();
+    // obsolete: adc_power_release();
     return cali_enable;
   }
-}
+}  // namespace webserver
