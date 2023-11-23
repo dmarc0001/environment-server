84c1271ffddc9c58675176e24c96f006b33978a3
Dirk Marciniak <dirk.marciniak@mail.de>
2023-11-23 18:18:43 +0100

    tool correct

--- tools/makeSPIFF.sh
+++ tools/makeSPIFF.sh
@@ -4,5 +4,8 @@
 IDF_PATH=/root/.platformio/packages/framework-espidf
 
 
+echo "make spiffs file..."
 $IDF_PATH/components/spiffs/spiffsgen.py 1536000 ../data spiffs-www.bin
+
+echo "upload spiffs..."
 esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 write_flash --flash_mode dio --flash_freq 40m  0x190000 spiffs-www.bin
